4ad0dc6f78dd1b46fb06a98815f38ecd7be23ec4
Brandon Bruno <bmbruno@gmail.com>
2022-11-01 21:12:52 -0400

    Asded any() example.

--- examples/any.js
+++ examples/any.js
@@ -8,19 +8,37 @@
 
             Promise.any()
 
-            TEXT
+            Given an array of promises, any() will return the first fulfilled promise.
 
         */
 
-        let promise = new Promise((resolve, reject) => {
+            // This promise will fulfill second (1500ms) and has no impact on any()
+            let promiseOne = new Promise((resolve) => { 
 
-            if (true) {
-                resolve("RESOLVED!");
-            } else {
-                reject("REJECTED!");
-            }
-        
-        });
+                setTimeout(() => {
+                    resolve("Promise One resolved!"); 
+                }, 1500);
+                
+            });
+
+            // This promise will fulfill first (750ms) and trigger any() to resolve
+            let promiseTwo = new Promise((resolve) => { 
+
+                setTimeout(() => {
+                    resolve("Promise Two resolved!"); 
+                }, 750);
+                
+            });
+
+            // This will reject right away, but won't have any effect on any(), since it only cares about fulfilled promises
+            let promiseThree = new Promise((resolve, reject) => { reject("Three rejected!"); });
+    
+            //
+            // Output the results (status, value, reason) of each settled promise
+            //
+    
+            Promise.any([promiseOne, promiseTwo, promiseThree])
+                .then(result => console.log(result));
 
 
     }
