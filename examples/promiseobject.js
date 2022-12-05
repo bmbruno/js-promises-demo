58a1a197cbbc6e445c9b3d91a56fb63f13f7c730
Brandon Bruno <bmbruno@gmail.com>
2022-12-05 14:08:27 -0500

    Added simple HTTP-type example to promiseobject.

--- examples/promiseobject.js
+++ examples/promiseobject.js
@@ -11,52 +11,44 @@
         */
 
         //
-        // Support functions
+        // Support function(s)
         //
 
-        // A largers 'baseNumber' value will significantly extend the time this fucntions takes
-        function DoLotsOfWork(baseNumber) {
+        function DoLotsOfWork() {
 
-            let startTime = performance.now();
-
-            // Borrowed from: https://gist.github.com/sqren/5083d73f184acae0c5b7
-            let result = 0;	
-            for (var i = Math.pow(baseNumber, 7); i >= 0; i--) {		
-                result += Math.atan(i) * Math.tan(i);
-            };
-
-            // Fake an occasional error (return negative status code)
-            if (Math.random() > 0.5) {
-                return -1;
-            }
-
-            let endTime = performance.now();
-
-            // Return execution time to indicate success
-            return (endTime - startTime);
+            // Simulate an HTTP 200 or 404 status code
+            return (Math.random() > 0.5) ? 200 : 404;
 
         };
 
         //
-        // Create a new Promise for later use
+        // Create a new Promise 
         //
 
-        const myPromise = new Promise( ( resolve, reject ) => {
+        // The Promise() object takes a function with two params: resolve, reject
+        let myPromise = new Promise( ( resolve, reject ) => {
 
-            // Start a long-running process
-            let result = DoLotsOfWork(11);
+            // Start a process without a definitive completion time
+            // This might be a network call, for example
+            let result = DoLotsOfWork();
 
-            if (result > 0)
+            // We determine what is considered a success or failure
+            // This could be anything; for example, if HTTP 200 was returned from a network call
+            if (result == 200)
                 resolve(result);
             else
-                reject("Code: " + result);
+                reject("HTTP " + result);
         });
 
-        console.log("Starting lots of work...");
+        //
+        // Execute the promise (note the order of output in the console)
+        //
+
+        console.log("Starting some task that will take time...");
 
         myPromise
-            .then(result => console.log(result))
-            .catch(error => console.log("ERROR: " + error));
+            .then(status => console.log("OK - " + status))
+            .catch(error => console.log("ERROR - " + error));
         
         console.log("Hanging out while that happens.");
 
